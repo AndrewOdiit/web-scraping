020f90630fc6dafa2032d0cc810e29879b4106f4
AndrewOdiit <andrewodiit2020@gmail.com>
2020-02-26 16:21:26 +0300

    Task 3 python implementation

--- task3/script.py
+++ task3/script.py
@@ -13,7 +13,7 @@ def get_data(page,user_agent, headers,cookies):
     # makes requests for page data
     print("User Agent: ", user_agent)
     session = requests.Session()
-    session.headers.update({'User-Agent':user_agent})
+    headers.update({'User-Agent':user_agent})
     try:
         assert page is not None
         response = session.get(
@@ -34,11 +34,13 @@ def get_x_path_data(response):
     if response.status_code == 200:
         
         tree = html.fromstring(response.content)
+        assert tree is not None
+        return tree.xpath("//body/script[5]/text()")[0]
     else:
         print("error occured: ", response.status_code)
+        return 0
 
-    assert tree is not None
-    return tree.xpath("//body/script[5]/text()")[0]
+    
 
 
 def extract_and_save_annonce(data:list, page:int, fields_dict:list,csv_writer): 
@@ -166,59 +168,9 @@ if __name__ == "__main__":
           ]
 
 
-    cookies = {
-        '_gcl_au': '1.1.1572045614.1581602136',
-        'cikneeto_uuid': 'id:fa1d0547-1bc4-4b71-8f3f-66daa7802b71',
-        'xtvrn': '$562498$',
-        'xtan562498': '-undefined',
-        'xtant562498': '1',
-        'ry_ry-l3b0nco_realytics': 'eyJpZCI6InJ5X0E4Rjk1NTU1LTAzRjQtNDI5NC1BNzVCLTk5QkJCQkFDQTU1QyIsImNpZCI6bnVsbCwiZXhwIjoxNjEzMTM4MTQ1Mzc0LCJjcyI6bnVsbH0%3D',
-        'saveOnboarding': '1',
-        'cookieFrame': '2',
-        'atidvisitor': '%7B%22name%22%3A%22atidvisitor%22%2C%22val%22%3A%7B%22vrn%22%3A%22-562498-%22%2C%22an%22%3A%22NaN%22%2C%22ac%22%3A%220%22%7D%2C%22options%22%3A%7B%22path%22%3A%22%2F%22%2C%22session%22%3A15724800%2C%22end%22%3A15724800%7D%7D',
-        '_fbp': 'fb.1.1581918967107.705022993',
-        '__gads': 'ID=4e95566f9daf183a:T=1581918989:S=ALNI_MaJRroLal4D6jRlXiTV-hZ62ptmVg',
-        'trc_cookie_storage': 'taboola%2520global%253Auser-id%3D48b945ef-5eb1-448f-bb7d-cfb45504236c-tuct4baa1ce',
-        'abtest_user': '1',
-        'nextSF': '1',
-        'didomi_token': 'eyJ1c2VyX2lkIjoiMTcwM2VkNTYtOTBkZi02ZWQ5LWE5ODgtNWFmNWExY2IxYmM1IiwiY3JlYXRlZCI6IjIwMjAtMDItMTNUMTM6NTU6MzguNDU3WiIsInVwZGF0ZWQiOiIyMDIwLTAyLTI1VDA4OjU5OjUxLjg4MFoiLCJ2ZW5kb3JzIjp7ImVuYWJsZWQiOlsiZ29vZ2xlIiwiYW1hem9uIl0sImRpc2FibGVkIjpbXX0sInB1cnBvc2VzIjp7ImVuYWJsZWQiOlsiY29va2llcyIsImFkdmVydGlzaW5nX3BlcnNvbmFsaXphdGlvbiIsImFkX2RlbGl2ZXJ5IiwiY29udGVudF9wZXJzb25hbGl6YXRpb24iLCJhbmFseXRpY3MiXSwiZGlzYWJsZWQiOltdfX0=',
-        'euconsent': 'BOutVWIOvUNROAHABBFRC--AAAAuhr_7__7-_9_-_f__9uj3Or_v_f__32ccL59v_h_7v-_7fi_20nV4u_1vft9yfk1-5ctDztp507iakivXmqdeb9v_nz3_5pxPr8k89r7337Ew_v8_v-b7BCON_IAAAA',
-        'ABTasty': 'uid%3D20021711084238872%26fst%3D1581926922538%26pst%3D1582626890329%26cst%3D1582719291371%26ns%3D19%26pvt%3D59%26pvis%3D1%26th%3D',
-        'datadome': 'zcxI-Mpn-f~cGEkHEXKOod-i3LdIqha6sxwB.ytNh18q.KfsGi.0qw4IcpTzMxWznEaGAA53eiVCSX~8Q0iRiA7S4uYXC~0inMLfti6cZ',
-        'cto_bundle': 'yPSNWF9sJTJCMkFYTHp2NlJ2YVZHYW5KS0dwaVV3MXdWV3h6bjZYM3hEMWMwNWhJdE1JZkNPJTJGZDdDSUgxbE9BeGRGZnpEYXlOaTdSa2klMkJNaWh1aW0lMkJNT0VWek1xWDMweDVCd0ZwMVJubXNRQW04SGtXeDBkdzZpNnNrRjlXaGRrZTliMFp5enh6YWxyUERleFZCTSUyRnlja2RPOU53JTNEJTNE',
-        'ry_ry-l3b0nco_so_realytics': 'eyJpZCI6InJ5X0E4Rjk1NTU1LTAzRjQtNDI5NC1BNzVCLTk5QkJCQkFDQTU1QyIsImNpZCI6bnVsbCwib3JpZ2luIjp0cnVlLCJyZWYiOm51bGwsImNvbnQiOm51bGwsIm5zIjpmYWxzZX0%3D',
-        '_pulse2data': '92e83110-2d91-4bb8-8310-02572f95d277%2Cv%2C%2C1582720215894%2CeyJpc3N1ZWRBdCI6IjIwMjAtMDItMTNUMTM6NTU6NDdaIiwiZW5jIjoiQTEyOENCQy1IUzI1NiIsImFsZyI6ImRpciIsImtpZCI6IjIifQ.._VCpjWRnSrYurwXLXxl8rg.sSvaRfmplyBGej7Z5E9BSxMkUdsTS-GYkjqt_wK2UT37kNes7rwszuMTwWM4jo1q-wJJGCmLajmB9CbZx8HXi13o-2ktlB0Zi33EB_33tTax8QWpsAG3GWJaBvWgfzkhfaIT5YN_igRwl9a-TyEcfbZK7uspv57-JwFLvDL83lzshLyfZgENkMwMvkBE9qDJQ4OpW0cDXhYztwsC27oS-Q.VxyO1Fz9HvYBDrYN_zrzsA%2C%2C0%2Ctrue%2C%2CeyJraWQiOiIyIiwiYWxnIjoiSFMyNTYifQ..QskdBilJSmSTMFEWFLumK_NubzQtVCX0rjiNEwtL73k',
-        'ABTastySession': 'sen%3D5__referrer%3D__landingPage%3Dhttps%3A//www.leboncoin.fr/recherche/%3Fcategory%3D9%26locations%3DCassis%5C_13260%26page%3D1',
-        'utag_main': 'v_id:01703ed577b70017f38b29907dac03069004506100868$_sn:27$_ss:1$_st:1582721658845$_pn:1%3Bexp-session$ses_id:1582719301547%3Bexp-session',
-        'cikneeto': 'date:1582719859276',
-    }
+    cookies = { }
 
-    headers = {
-        'Connection': 'keep-alive',
-        'Cache-Control': 'max-age=0',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Mobile Safari/537.36',
-        'Sec-Fetch-Dest': 'document',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Sec-Fetch-Site': 'none',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-User': '?1',
-        'Accept-Language': 'en-US,en-GB;q=0.9,en;q=0.8,fr;q=0.7',
-    }
-
-    params = (
-        ('category', '9'),
-        ('locations', 'Cassis_13260'),
-        ('page', '1'),
-    )
-
-    response = requests.get('https://www.leboncoin.fr/recherche/', headers=headers, params=params, cookies=cookies)
-
-    #NB. Original query string below. It seems impossible to parse and
-    #reproduce query strings 100% accurately so the one below is given
-    #in case the reproduced version is not "correct".
-    # response = requests.get('https://www.leboncoin.fr/recherche/?category=9&locations=Cassis_13260&page=1', headers=headers, cookies=cookies)
-
+    headers = { }
 
     page_count = 1 #used as counter and passed as url  page parameter
     csv_writer = Csvwriter('output.csv')
