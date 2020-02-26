d91be99fa2a1ecd3c0cf505e39c3a1fb8f5c09bf
AndrewOdiit <andrewodiit2020@gmail.com>
2020-02-26 21:13:00 +0300

    Task 3 Python implementation

--- task3/script.py
+++ task3/script.py
@@ -178,44 +178,9 @@ target_fields = [
 if __name__ == "__main__":
     visited = [] #This list will hold all visited urls
 
-    cookies = {
-        '_gcl_au': '1.1.1572045614.1581602136',
-        'cikneeto_uuid': 'id:fa1d0547-1bc4-4b71-8f3f-66daa7802b71',
-        'xtvrn': '$562498$',
-        'xtan562498': '-undefined',
-        'xtant562498': '1',
-        'ry_ry-l3b0nco_realytics': 'eyJpZCI6InJ5X0E4Rjk1NTU1LTAzRjQtNDI5NC1BNzVCLTk5QkJCQkFDQTU1QyIsImNpZCI6bnVsbCwiZXhwIjoxNjEzMTM4MTQ1Mzc0LCJjcyI6bnVsbH0%3D',
-        'saveOnboarding': '1',
-        'cookieFrame': '2',
-        'atidvisitor': '%7B%22name%22%3A%22atidvisitor%22%2C%22val%22%3A%7B%22vrn%22%3A%22-562498-%22%2C%22an%22%3A%22NaN%22%2C%22ac%22%3A%220%22%7D%2C%22options%22%3A%7B%22path%22%3A%22%2F%22%2C%22session%22%3A15724800%2C%22end%22%3A15724800%7D%7D',
-        '_fbp': 'fb.1.1581918967107.705022993',
-        '__gads': 'ID=4e95566f9daf183a:T=1581918989:S=ALNI_MaJRroLal4D6jRlXiTV-hZ62ptmVg',
-        'trc_cookie_storage': 'taboola%2520global%253Auser-id%3D48b945ef-5eb1-448f-bb7d-cfb45504236c-tuct4baa1ce',
-        'abtest_user': '1',
-        'nextSF': '1',
-        'didomi_token': 'eyJ1c2VyX2lkIjoiMTcwM2VkNTYtOTBkZi02ZWQ5LWE5ODgtNWFmNWExY2IxYmM1IiwiY3JlYXRlZCI6IjIwMjAtMDItMTNUMTM6NTU6MzguNDU3WiIsInVwZGF0ZWQiOiIyMDIwLTAyLTI1VDA4OjU5OjUxLjg4MFoiLCJ2ZW5kb3JzIjp7ImVuYWJsZWQiOlsiZ29vZ2xlIiwiYW1hem9uIl0sImRpc2FibGVkIjpbXX0sInB1cnBvc2VzIjp7ImVuYWJsZWQiOlsiY29va2llcyIsImFkdmVydGlzaW5nX3BlcnNvbmFsaXphdGlvbiIsImFkX2RlbGl2ZXJ5IiwiY29udGVudF9wZXJzb25hbGl6YXRpb24iLCJhbmFseXRpY3MiXSwiZGlzYWJsZWQiOltdfX0=',
-        'euconsent': 'BOutVWIOvUNROAHABBFRC--AAAAuhr_7__7-_9_-_f__9uj3Or_v_f__32ccL59v_h_7v-_7fi_20nV4u_1vft9yfk1-5ctDztp507iakivXmqdeb9v_nz3_5pxPr8k89r7337Ew_v8_v-b7BCON_IAAAA',
-        'ABTasty': 'uid%3D20021711084238872%26fst%3D1581926922538%26pst%3D1582626890329%26cst%3D1582719291371%26ns%3D19%26pvt%3D60%26pvis%3D2%26th%3D',
-        'cto_bundle': '62rSjl9sJTJCMkFYTHp2NlJ2YVZHYW5KS0dwaWFnY3ZFZ2J3Uzg2S0clMkZJakpFM3gwJTJGZHg4aHl2QWUzZXNuc01CazVYZ2xnTmlFWklMSG1pNzdjZFhLTjQwNEtBNTVrV2ZMV1IxQkUzY29mZ2pOTGZUWFIlMkZJbjFEM3VFQ3dnV1dhbm1HSFhjRHh5cUNreFRnalVqSXAyTSUyQm96TWxBJTNEJTNE',
-        'datadome': 'TVSqjOIfxR1jviaqmRrkceCXQtob0~TuNmhwRFHzyGoI~7GKP3g6J_xX.XKDBjcJovQYIfY-xFlR9NkO8pwL.wmd4K2v0kCcq~yJV7RS0q',
-        'ry_ry-l3b0nco_so_realytics': 'eyJpZCI6InJ5X0E4Rjk1NTU1LTAzRjQtNDI5NC1BNzVCLTk5QkJCQkFDQTU1QyIsImNpZCI6bnVsbCwib3JpZ2luIjpmYWxzZSwicmVmIjpudWxsLCJjb250IjpudWxsLCJucyI6ZmFsc2V9',
-        '_pulse2data': '92e83110-2d91-4bb8-8310-02572f95d277%2Cv%2C%2C1582729961454%2CeyJpc3N1ZWRBdCI6IjIwMjAtMDItMTNUMTM6NTU6NDdaIiwiZW5jIjoiQTEyOENCQy1IUzI1NiIsImFsZyI6ImRpciIsImtpZCI6IjIifQ..AqHLNoDDrRhSNPshEgJLVQ.PVPolWL3CcUEuFh1VYGsEXBv8iRiDxGTGjAq-NyQIZEUiKK09thY3kDSamvKFV-OhKgi-UIVen70O9w4niT8WU4vk-W1LivC3_tMWLoUyIv8_J6lG-L_Ayd2c8m7dfEKGgzmEFC2vZhRUZyZq49RF70hY4gjQ6HqcbK9mv1OzjAmsNkCr1OX77B6Cb4JfEbFFjGaei3DgdffjS_IarM0Vg.B2MmhpwDl52gukSAGkNqPQ%2C%2C0%2Ctrue%2C%2CeyJraWQiOiIyIiwiYWxnIjoiSFMyNTYifQ..QskdBilJSmSTMFEWFLumK_NubzQtVCX0rjiNEwtL73k',
-        'utag_main': 'v_id:01703ed577b70017f38b29907dac03069004506100868$_sn:28$_ss:1$_st:1582730938845$_pn:1%3Bexp-session$ses_id:1582729051265%3Bexp-session',
-        'cikneeto': 'date:1582729139631',
-    }
+    cookies = {} #PLEASE PROVIDE COOKIES
 
-    headers = {
-        'Connection': 'keep-alive',
-        'Cache-Control': 'max-age=0',
-        'Upgrade-Insecure-Requests': '1',
-        'Sec-Fetch-Dest': 'document',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Sec-Fetch-Site': 'cross-site',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-User': '?1',
-        'Referer': 'https://c.captcha-delivery.com/captcha/?initialCid=AHrlqAAAAAMAexFMm74NXJQAxe3ydQ%3D%3D&hash=05B30BD9055986BD2EE8F5A199D973&cid=PlWe2edNZpOFxA-nKNviYOk5RVKjEJyHstqAN1bR17-q3smOhOG4o24MreUcYNDt3EAcaI2hXvff~e5AOjkjmbFXFvGS2n9NX3DvKb_W_~&t=fe',
-        'Accept-Language': 'en-US,en-GB;q=0.9,en;q=0.8,fr;q=0.7',
-    }
+    headers = {} #PLEASE PROVIDE HEADERS
     
     
     if len(cookies) < 1 or len(headers) < 1:
